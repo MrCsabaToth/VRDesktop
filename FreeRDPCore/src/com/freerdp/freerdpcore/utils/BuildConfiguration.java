77702c07466cc5fff401e9d7df28dfc0bf041eaf
Csaba Toth <tocsa@atoth.sote.hu>
2016-01-17 18:37:27 -0800

    Sources

---
+++ FreeRDPCore/src/com/freerdp/freerdpcore/utils/BuildConfiguration.java
@@ -0,0 +1,15 @@
+package com.freerdp.freerdpcore.utils;
+
+public class BuildConfiguration {
+	public static final String WITH_ANDROID_DEBUG_MENU = "OFF";
+
+	public static final int FREERDP_VERSION_MAJOR = 2;
+	public static final int FREERDP_VERSION_MINOR = 0;
+	public static final int FREERDP_VERSION_REVISION = 0;
+
+	public static final String FREERDP_VERSION_SUFFIX = "dev";
+	public static final String FREERDP_API_VERSION = "2.0";
+	public static final String FREERDP_VERSION = "2.0.0";
+	public static final String FREERDP_VERSION_FULL = "2.0.0-dev";
+	public static final String GIT_REVISION = "df81e84";
+	}
